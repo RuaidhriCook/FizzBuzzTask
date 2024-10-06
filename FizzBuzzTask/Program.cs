9d8536e574b097ab725f62e6b874f1ddd66e986d
RuaidhriCook <Ruaidhri8@gmail.com>
2024-10-06 15:54:23 +0100

    Implemented console app to run the 1-100 range

--- FizzBuzzTask/Program.cs
+++ FizzBuzzTask/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzzTask.Interfaces;
+using FizzBuzzTask.Invokers;
+using FizzBuzzTask.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IFizzBuzzService fizzBuzzService = new FizzBuzzService();
+        var invoker = new FizzBuzzReflectionInvoker(fizzBuzzService);
+
+        var fizzBuzzMethodName = "GetFizzBuzzResult";
+
+        for (int i = 1; i <= 100; i++)
+        {
+            var result = invoker.InvokeFizzBuzz(i, fizzBuzzMethodName);
+            if (result.IsSuccess)
+                Console.WriteLine(result.Value);
+            else
+                Console.WriteLine("Error Getting Result");
+        }
+
+        Console.ReadLine();
+    }
+}
