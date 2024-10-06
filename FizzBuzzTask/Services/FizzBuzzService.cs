067178d3c1e7bd8ef4d3b43850c26130b35abb2d
RuaidhriCook <Ruaidhri8@gmail.com>
2024-10-06 15:14:46 +0100

    Added code to pass 'FizzBuzz' test case (divisble by 3 and 5)

--- FizzBuzzTask/Services/FizzBuzzService.cs
+++ FizzBuzzTask/Services/FizzBuzzService.cs
@@ -6,10 +6,15 @@ namespace FizzBuzzTask.Services
     {
         public string GetFizzBuzzResult(int number)
         {
+            if (number % 15 == 0)
+                return "FizzBuzz";
+
             if (number % 3 == 0)
                 return "Fizz";
+
             if (number % 5 == 0)
                 return "Buzz";
+
             return number.ToString();
         }
     }
